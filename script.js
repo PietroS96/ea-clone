ce8ba7416fc5dc0eef8c125e576ca89b83005ac4
Pietro Scaduto <pietro.scaduto25@gmail.com>
2021-12-03 11:32:43 +0100

    feat:update

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-let leftMenu = document.getElementById('show-menu');
-let circleBtn = document.getElementById('circle-btn');
-let menuCloser = document.getElementById('menu-closer')
-let menu = 'close'
-function aside (){
-    if(menu == 'close'){
-        leftMenu.style.left = 0;
-        menu = 'open';
-    }else{
-        leftMenu.style.left = '-500px';
-        menu = 'close'
-    }
+let leftMenu = document.getElementById("show-menu");
+let circleBtn = document.getElementById("circle-btn");
+let menuCloser = document.getElementById("menu-closer");
+let menu = "close";
+function aside() {
+  if (menu == "close") {
+    leftMenu.style.left = 0;
+    menu = "open";
+  } else {
+    leftMenu.style.left = "-500px";
+    menu = "close";
+  }
 }
 
-circleBtn.addEventListener('click', ()=>{
-    aside()
-})
+circleBtn.addEventListener("click", () => {
+  aside();
+});
 
-menuCloser.addEventListener('click', ()=>{
-    aside()
-})
+menuCloser.addEventListener("click", () => {
+  aside();
+});
+
+function showMenu() {
+  document.getElementById("panel-active").classList.toggle("panel-hide");
+}
