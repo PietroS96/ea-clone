f49656d756377d9e7f1410cd3116e1ae738ebd51
Pietro Scaduto <pietro.scaduto25@gmail.com>
2022-01-13 17:47:07 +0100

    feat: update assets and merged branch

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-let leftMenu = document.getElementById("show-menu");
-let circleBtn = document.getElementById("circle-btn");
-let menuCloser = document.getElementById("menu-closer");
-let menu = "close";
-function aside() {
-  if (menu == "close") {
-    leftMenu.style.left = 0;
-    menu = "open";
-  } else {
-    leftMenu.style.left = "-500px";
-    menu = "close";
-  }
+let leftMenu = document.getElementById('show-menu');
+let circleBtn = document.getElementById('circle-btn');
+let menuCloser = document.getElementById('menu-closer')
+let menu = 'close'
+function aside (){
+    if(menu == 'close'){
+        leftMenu.style.left = 0;
+        menu = 'open';
+        leftMenu.style.boxShadow = '50px 4px 24px 100000rem #161616d8';
+        leftMenu.style.transition = '0.8s'
+        document.querySelector('header').style.position = 'fixed-top'
+    
+    }else{
+        leftMenu.style.left = '-500px';
+        menu = 'close'
+        leftMenu.style.boxShadow = 'none'
+    }
 }
 
-circleBtn.addEventListener("click", () => {
-  aside();
-});
+circleBtn.addEventListener('click', ()=>{
+    aside()
+})
 
-menuCloser.addEventListener("click", () => {
-  aside();
-});
+menuCloser.addEventListener('click', ()=>{
+    aside()
+})
 
 function showMenu() {
   document.getElementById("panel-active").classList.toggle("panel-hide");
